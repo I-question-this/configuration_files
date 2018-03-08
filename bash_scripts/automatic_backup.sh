5bdf519facc7853651ed3771c27d2119fd5ce52f
Tyler Westland <tylerofthewest@gmail.com>
2018-03-08 08:41:18 -0600

    Made unison logfile path a variable. Added logfile for VBoxManage calls

--- bash_scripts/automatic_backup.sh
+++ bash_scripts/automatic_backup.sh
@@ -3,13 +3,17 @@
 # Create the currebt date
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
+# Logfiles
+UNISON_LOGFILE="/var/log/unison/home_to_backup.log"
+VBOX_LOGFILE="/var/log/vbox/git_server_snapshot.log"
+
 # VirtualBox Daily SnapShot
-VBoxManage snapshot "git_server" take "DailySnapShot -- $DATE" --description "Daily Snap Shot -- $DATE"
+VBoxManage snapshot "git_server" take "DailySnapShot -- $DATE" --description "Daily Snap Shot -- $DATE" &>> $VBOX_LOGFILE 
 
 # Remove Extra Daily SnapShots
 for uuid in $(vbox_manage_extra_daily_snapshots.py "git_server");
-    do VBoxManage snapshot "git_server" delete $uuid;
+    do VBoxManage snapshot "git_server" delete $uuid &>> $VBOX_LOGFILE;
 done
 
 # Unison back up of hone directory
-unison home_to_backup -auto -batch -silent -logfile /var/log/unison/home_to_backup.log
+unison home_to_backup -auto -batch -silent -logfile $UNISON_LOGFILE
