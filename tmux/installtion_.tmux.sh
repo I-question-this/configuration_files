09c0990a4542f9eecbfa64c62d754a7b51319736
Tyler Westland <tylerofthewest@gmail.com>
2018-11-10 09:38:31 -0500

    Fixes tmux installation link paths
    The links were not using realpath so they were faulty

--- tmux/installtion_.tmux.sh
+++ tmux/installtion_.tmux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # make link to .tmux.conf
-ln -s -f .tmux/.tmux.conf $HOME
+ln -s -f $(realpath .tmux/.tmux.conf) $HOME/.tmux.conf
 # make link to my edits to the .tmux.conf.local
-ln -s -f .tmux.conf.local $HOME
+ln -s -f $(realpath .tmux.conf.local) $HOME/.tmux.conf.local
