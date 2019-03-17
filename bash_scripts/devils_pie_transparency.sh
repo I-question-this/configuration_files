4dbbf253df1dd9ab1ee3c85b7f10afc0ca2386c3
Tyler Westland <tylerofthewest@gmail.com>
2019-03-17 13:24:59 -0400

    Adds script to toggle transparency
    Rewrites devils pie script and reruns devils pie

---
+++ bash_scripts/devils_pie_transparency.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+config=$HOME/.devilspie/transparency_config.ds
+
+transparent=0xdfffffff
+opaque=0xffffffff
+ 
+if grep -Fq "$transparent" "$config"; then
+  transparency=$opaque
+else
+  transparency=$transparent
+fi
+
+cat > $config << END
+  (begin
+    (spawn_async (str "xprop -id " (window_xid) " -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY $transparency"))
+  )
+END
+
+devilspie &
