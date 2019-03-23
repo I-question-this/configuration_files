f0870d9a1cbb25d744d0e9f9f61491e0fe692416
Tyler Westland <tylerofthewest@gmail.com>
2019-03-23 11:51:44 -0400

    Bug fix for disarm_directory_of_pdfs.sh
    It was missing " marks.

--- bash_scripts/disarm_directory_of_pdfs.sh
+++ bash_scripts/disarm_directory_of_pdfs.sh
@@ -3,4 +3,4 @@
 # This uses Dieter Steven's pdfid.py to disarm pdfs
 # This uses parameter expansion 
 
-find . -type f -execdir bash -c 'pdfid.py -d $0;mv ${0%pdf*}disarmed.pdf $0' {} \;
+find . -type f -execdir bash -c 'pdfid.py -d "$0";mv "${0%pdf*}disarmed.pdf" "$0"' {} \;
