8c0053379dd56c846759f0d65cd9369fd318c9aa
Tyler Westland <tylerofthewest@gmail.com>
2019-09-20 10:36:29 -0400

    Removes previously created pixelated screenshot.

--- bash_scripts/pixelated_screenshot.sh
+++ bash_scripts/pixelated_screenshot.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # Code from: hund0b1 on github.com
 
-
+# Define the path to the image
 img="$HOME/.cache/pixelated_screenshot.png"
+
+# Delete the previously taken image
+rm -f $img
 
 # Take screenshot
 scrot $img
 # Pixelate image
 convert $img -scale 10% -scale 1000% $img
 
+# Print the path to the created image
 echo $img
