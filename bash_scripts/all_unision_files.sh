5c8199ca1b37515d0c20b97cccd24b23a48651fd
Tyler Westland <tylerofthewest@gmail.com>
2019-10-19 14:47:49 -0400

    Makes all unison processes simultaneous.

--- bash_scripts/all_unision_files.sh
+++ bash_scripts/all_unision_files.sh
@@ -3,5 +3,5 @@
 for preference_file in $(ls $HOME/.unison | grep prf)
 do
 	filtered_preference_file=$(echo $preference_file | sed 's/.prf//g')
-	unison -auto -batch $filtered_preference_file
+	unison -auto -batch $filtered_preference_file &
 done
