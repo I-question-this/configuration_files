beb388e97f871b20756268a10c2d08369f30aed6
Tyler Westland <tylerofthewest@gmail.com>
2020-03-04 19:13:48 -0500

    Adds protection to change background.
    Pywal breaks radare2, so I don't want it to
    run if radare2 is running.

--- bash_scripts/change_background.sh
+++ bash_scripts/change_background.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
-WALLPAPERS=$HOME/Pictures/Wallpapers
-wal --vte -qi $WALLPAPERS
+function is_r2_running() {
+  if pgrep -x "radare2" > /dev/null || pgrep -x "r2" > /dev/null
+  then
+    return 0
+  else
+    return 1
+  fi
+}
+
+if ! is_r2_running
+then
+  WALLPAPERS=$HOME/Pictures/Wallpapers
+  wal --vte -qi $WALLPAPERS
+else
+  echo -n "pywal breaks radare2, please close it gracefully so pywal doesn't "
+  echo "kill it."
+fi
