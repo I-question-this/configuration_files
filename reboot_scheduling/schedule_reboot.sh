e2639977b23853bb30b6959e45c3fd7889e0aa36
Tyler Westland <tylerofthewest@gmail.com>
2020-03-17 12:22:31 -0400

    Adds option to specify a different date.

--- reboot_scheduling/schedule_reboot.sh
+++ reboot_scheduling/schedule_reboot.sh
@@ -5,8 +5,17 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+if [ "$#" -ge 2 ]; then
+  echo "$0: <date to shutdown>"
+  exit 1
+elif [ "$#" -eq 1 ]; then
+  target_date=$1
+else
+  target_date="next Sunday"
+fi
+
 # Calculate minutes to reboot
-reboot_date_seconds=$(date -d "next Sunday" "+%s")
+reboot_date_seconds=$(date -d "$target_date" "+%s")
 now_date_seconds=$(date -d "now" "+%s")
 minutes_til_reboot=$(echo "($reboot_date_seconds - $now_date_seconds)/60.0" | bc)
 # Schedule the reboot
