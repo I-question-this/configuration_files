dfacc3cfff461fbdc00f66186a58dcd6213f8a44
Tyler Westland <tylerofthewest@gmail.com>
2020-08-02 17:10:27 -0400

    Adds more aliases.

--- oh-my-zsh/custom/metasm.zsh
+++ oh-my-zsh/custom/metasm.zsh
@@ -2,3 +2,5 @@
 metasm_path=$HOME/Development/Ruby/metasm/samples
 alias disassemble="ruby $metasm_path/disassemble.rb"
 alias elfencode="ruby $metasm_path/elfencode.rb"
+alias exeencode="ruby $metasm_path/exeencode.rb"
+alias peencode="ruby $metasm_path/peencode.rb"
