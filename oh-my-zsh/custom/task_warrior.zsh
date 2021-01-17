3084fea092fd311bca69976f43740dbe679d0561
Tyler Westland <tylerofthewest@gmail.com>
2021-01-17 08:15:21 -0600

    Adds more task warrior aliases.

--- oh-my-zsh/custom/task_warrior.zsh
+++ oh-my-zsh/custom/task_warrior.zsh
@@ -1,3 +1,7 @@
 # Task Warrior
 alias taskUnBlocked="task +UNBLOCKED next"
 alias taskFocused="task +UNBLOCKED -Hobby next"
+alias taskWork="task +UNBLOCKED +Work next"
+alias taskSchool="task +UNBLOCKED +School next"
+alias taskHobby="task +UNBLOCKED +Hobby next"
+alias taskChores="task +UNBLOCKED +Chores next"
