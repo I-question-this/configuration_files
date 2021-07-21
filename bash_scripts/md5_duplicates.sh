a74ef3c591dd728399fd35cad38aea140bb87805
Tyler Westland <tylerofthewest@gmail.com>
2021-07-21 10:26:34 -0400

    Adds small utility script for finding md5 dups.
    I don't remember why I thought this was important
    actually.

---
+++ bash_scripts/md5_duplicates.sh
@@ -0,0 +1,6 @@
+#\bin/bash
+# From https://stackoverflow.com/a/19552048
+
+find . -not -empty -type f -printf "%s\n" | sort -rn | uniq -d |\
+xargs -I{} -n1 find . -type f -size {}c -print0 | xargs -0 md5sum |\
+sort | uniq -w32 --all-repeated=separate
