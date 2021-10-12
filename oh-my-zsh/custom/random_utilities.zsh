5d1edfa162913d995a1e57d8cb00d0e91715b44f
Tyler Westland <tylerofthewest@gmail.com>
2021-10-12 11:17:40 -0400

    Adds do_echo
    Echos and then runs the command! I find this helpful to run in loops
    that I'm constructing but there are certainly endless!

--- oh-my-zsh/custom/random_utilities.zsh
+++ oh-my-zsh/custom/random_utilities.zsh
@@ -6,10 +6,13 @@ alias istheinternetonfire="dig +short -t txt istheinternetonfire.com"
 alias recursive_touch="find . -exec touch {} \;"
 alias simpleHTTPServer="python2 -m SimpleHTTPServer"
 alias graphics_card="lspci -k | grep -A 2 -E '(VGA|3D)'"
+alias update_rawdog_webpage="cat /home/tyler/.rawdog/output.html | tee /srv/html/rawdog.html"
 
 # nohup -- Make it quiet.
 # disown -- Make it not die with when the terminal is closed.
 function independent_child () { nohup $@ &> /dev/null & disown }
+function independent_zathura () { independent_child zathura $@ }
+
 function pkill--discord () { pkill -f /opt/discord/Discord; pkill -f /opt/discord/Discord}
 
-function independent_zathura () { independent_child zathura $@ }
+function do_echo () { echo $@; $@ }
