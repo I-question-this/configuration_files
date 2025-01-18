262182e418fc2c3559fe0c1b56488cfc6a52c5a5
Tyler Westland <tylerofthewest@gmail.com>
2025-01-18 14:54:27 -0600

    Don't know. Don't use tasker anymore

--- oh-my-zsh/custom/task_warrior.zsh
+++ oh-my-zsh/custom/task_warrior.zsh
@@ -1,7 +1,8 @@
 # Task Warrior
 alias taskUnBlocked="task +UNBLOCKED next"
-alias taskFocused="task +UNBLOCKED -Hobby next"
+alias taskFocused="task +UNBLOCKED next"
 alias taskWork="task +UNBLOCKED +Work next"
 alias taskSchool="task +UNBLOCKED +School next"
 alias taskHobby="task +UNBLOCKED +Hobby next"
 alias taskChores="task +UNBLOCKED +Chores next"
+alias taskEducationResearch="task +UNBLOCKED +EducationResearch next"
