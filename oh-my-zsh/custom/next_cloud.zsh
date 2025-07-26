89eec596e6607e1a6e58d5d578b0ae8b2db12f3b
Tyler Westland <tylerofthewest@gmail.com>
2025-07-26 13:59:38 -0500

    Improves nextcloud aliases

--- oh-my-zsh/custom/next_cloud.zsh
+++ oh-my-zsh/custom/next_cloud.zsh
@@ -1,2 +1,19 @@
 # https://github.com/nextcloud/all-in-one
-alias occ="sudo docker exec --user www-data -it nextcloud-aio-nextcloud php"
+nc-prod() {
+ (sudo docker exec --user www-data -it nextcloud-aio-nextcloud $@)
+}
+nc-prod-root() {
+ (sudo docker exec --user root -it nextcloud-aio-nextcloud $@)
+}
+alias nc-prod-occ="nc-prod php occ"
+alias nc-prod-bash="nc-prod bash"
+alias nc-prod-root-bash="nc-prod-root bash"
+
+# https://github.com/juliusknorr/nextcloud-docker-dev
+nc-dev() {
+ (cd ~/Development/NextCloud/nextcloud-docker-dev && sudo docker compose $@)
+}
+alias nc-dev-occ="nc-dev exec nextcloud occ"
+alias nc-dev-bash="nc-dev exec nextcloud bash"
+alias nc-dev-up="nc-dev up nextcloud"
+alias nc-dev-down="nc-dev down"
