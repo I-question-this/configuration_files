d5bff8cf9d26aecb11cdf1af98c5bd42a236d83d
Tyler Westland <tylerofthewest@gmail.com>
2025-07-28 11:20:03 -0500

    Adds 'grep-recursive' alias

--- oh-my-zsh/custom/random_utilities.zsh
+++ oh-my-zsh/custom/random_utilities.zsh
@@ -9,6 +9,7 @@ alias graphics_card="lspci -k | grep -A 2 -E '(VGA|3D)'"
 alias update_rawdog_webpage="cat /home/tyler/.rawdog/output.html | tee /srv/html/rawdog.html"
 alias start-ssh-agent='eval "$(ssh-agent -s)"'
 alias home_proxy="ssh -N -D 9090 raspberry-pi"
+alias grep-recursive="grep -R --exclude-dir='node_modules' --exclude-dir='.git' --exclude-dir='build' --exclude-dir='*cache' "
 
 # nohup -- Make it quiet.
 # disown -- Make it not die with when the terminal is closed.
