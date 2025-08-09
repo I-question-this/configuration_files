4fe2bcdf38adb2e7775e72d213f558981fbdf383
Tyler Westland <tyler.westland@proton.me>
2025-08-09 09:26:59 -0500

    Adds initialization of nvm

--- oh-my-zsh/custom/npm.sh
+++
@@ -1,3 +0,0 @@
-# Allows packages to be installed globally without sudo permissions.
-# This does restrict things to the single user.
-export PATH=$HOME/.npm-global/bin:$PATH
--- oh-my-zsh/custom/nvm.zsh
+++ oh-my-zsh/custom/nvm.zsh
@@ -1,4 +1,7 @@
 # NVM
+# Load up nvm
+. /usr/share/nvm/init-nvm.sh
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
