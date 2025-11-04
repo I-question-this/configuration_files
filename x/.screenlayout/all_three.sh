cc4acf605643b85d61584fc5c9f620ca8f0ea10f
Tyler Westland <tyler.westland@proton.me>
2025-11-03 20:44:29 -0600

    Adds screen layout scripts

---
+++ x/.screenlayout/all_three.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --primary --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DVI-D-1 --off --output DVI-I-2-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-3-3 --mode 1920x1080 --pos 0x1080 --rotate normal --output DVI-I-1-1 --off
---
+++ x/.screenlayout/only_desk--portrait.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DVI-D-1 --off --output DVI-I-1-1 --off --output DVI-I-2-2 --mode 1920x1080 --pos 0x0 --rotate right --output DVI-I-3-3 --primary --mode 1920x1080 --pos 1080x0 --rotate right
